7a5f0b4c34869134d036d74f5ceb1a49be4c9001
Rayen Hammami <rayenhammami748@gmail.com>
2025-01-09 14:12:58 +0100

    message

--- main/history.js
+++ main/history.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     function displayTimeline() {
         var timeline = $('#timeline')
-<<<<<<< HEAD
         
         for (var i = 0; i < playersData.length; i++) {
             var player = playersData[i]
@@ -14,26 +13,11 @@ $(document).ready(function() {
                 var history = player.careerHistory[j]
                 var careerItem = $('<div>').addClass('career-item')
                 
-=======
-
-        for (var i = 0; i < playersData.length; i++) {
-            var player = playersData[i]
-            var timelineEntry = $('<div>').addClass('timeline-entry')
-
-            var playerName = $('<h3>').text(player.name)
-            var careerTimeline = $('<div>').addClass('career-timeline')
-
-            for (var j = 0; j < player.careerHistory.length; j++) {
-                var history = player.careerHistory[j]
-                var careerItem = $('<div>').addClass('career-item')
-
->>>>>>> ad22705e9dc71de295f73d7d5936db13ab04d85f
                 // Add club logo
                 var logo = $('<img>')
                     .attr('src', history.logo)
                     .attr('alt', history.club + ' logo')
                     .addClass('club-logo')
-<<<<<<< HEAD
                 
                 var clubName = $('<h4>').text(history.club)
                 var years = $('<p>').text(history.years)
@@ -42,16 +26,6 @@ $(document).ready(function() {
                 careerTimeline.append(careerItem)
             }
             
-=======
-
-                var clubName = $('<h4>').text(history.club)
-                var years = $('<p>').text(history.years)
-
-                careerItem.append(logo, clubName, years)
-                careerTimeline.append(careerItem)
-            }
-
->>>>>>> ad22705e9dc71de295f73d7d5936db13ab04d85f
             timelineEntry.append(playerName, careerTimeline)
             timeline.append(timelineEntry)
         }
